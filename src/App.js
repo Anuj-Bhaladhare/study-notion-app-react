04f0f03f61d99fc658fc4153e7c1fdda49ba3636
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-08-04 10:50:09 +0530

    creating router in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Router } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-          Study Notion
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Router>
+           <Route path='/' element={<Home />}/>
+           <Route path='/dashboard' element={<Dashboard />}/>
+           <Route path='/login' element={<Login />}/>
+           <Route path='/signup' element={<Signup />}/>
+        </Router>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
-           Navbar 
+
+           <div>
+              <Link to="/">
+                  <img src="../assets/Logo.svg"/>
+              </Link>
+           </div>
+
+           <nav>
+              <ul>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/">About</NavLink></li>
+                <li><NavLink to="/">Contect</NavLink></li>
+              </ul>
+           </nav>
+
+           {/* Buttons  =>  Login, Log Out, Sign In, Dashboard */}
+
+           <div>
+              {
+                 <NavLink to="/login">
+                    <button>Log In</button>
+                 </NavLink>
+              }
+              {
+                 <NavLink to="/signup">
+                    <button>Sign Up</button>
+                 </NavLink>
+              }
+              {
+                 <NavLink to="/dashboard">
+                    <button>Dashboard</button>
+                 </NavLink>
+              }
+              {
+                 <NavLink to="/">
+                    <button>Log Out</button>
+                 </NavLink>
+              }
+           </div>
         </div>
     )
 }
