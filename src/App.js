55f0875a9eba32e8c505836fa6a5ebdb32977437
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-08-04 09:55:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <div className="flex justify-center align-center mt-[13rem]">
+          <h1 className=' font-bold text-[2rem]'>Tailwind CSS</h1>
+        </div>
+        <div className="flex justify-center align-center">
+          <img src='https://res.cloudinary.com/practicaldev/image/fetch/s--gDacVBBY--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_66%2Cw_880/https://thepracticaldev.s3.amazonaws.com/i/tqpthh0cv88anxyg48m0.gif'/>
+        </div>
+        <div className="flex justify-center align-center">
+          <p>Tailwind is a CSS framework for rapid-fire UI development</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
